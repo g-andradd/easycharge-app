c7498511b78424d3816ab7037f7f5a286b1e372c
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-06-29 17:17:09 -0300

    cadastro completo e model de clientes completo

--- lib/models/cliente.dart
+++ lib/models/cliente.dart
@@ -1,6 +1,31 @@
 class Cliente {
   final String nome;
   final String cpf;
+  final String telefone;
+  final String email;
+  final String rua;
+  final String numero;
+  final String? complemento;
+  final String bairro;
+  final String cidade;
+  final String estado;
+  final String profissao;
+  final double renda;
+  final String status;
 
-  Cliente(this.nome, this.cpf);
+  Cliente(
+    this.nome,
+    this.cpf,
+    this.telefone,
+    this.email,
+    this.rua,
+    this.numero,
+    this.complemento,
+    this.bairro,
+    this.cidade,
+    this.estado,
+    this.profissao,
+    this.renda,
+    this.status,
+  );
 }
--- lib/screens/formulario/cliente.dart
+++ lib/screens/formulario/cliente.dart
@@ -24,9 +24,20 @@ class FormularioCliente extends StatefulWidget {
 }
 
 class FormularioClienteState extends State<FormularioCliente> {
-  final TextEditingController _controladorCampoNome = TextEditingController();
-  final TextEditingController _controladorCampoCpf = TextEditingController();
   final _formkey = GlobalKey<FormState>();
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _cpfController = TextEditingController();
+  final TextEditingController _telefoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _ruaController = TextEditingController();
+  final TextEditingController _numeroController = TextEditingController();
+  final TextEditingController _complementoController = TextEditingController();
+  final TextEditingController _bairroController = TextEditingController();
+  final TextEditingController _cidadeController = TextEditingController();
+  final TextEditingController _estadoController = TextEditingController();
+  final TextEditingController _profissaoController = TextEditingController();
+  final TextEditingController _rendaController = TextEditingController();
+  final TextEditingController _statusController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -37,78 +48,196 @@ class FormularioClienteState extends State<FormularioCliente> {
       body: SingleChildScrollView(
         child: _construirFormulario(context),
       ),
-      backgroundColor: Theme
-          .of(context)
-          .secondaryHeaderColor,
+      backgroundColor: Theme.of(context).secondaryHeaderColor,
     );
   }
 
   void _criaCliente(BuildContext context) {
+    final String nome = _nomeController.text;
+    final String cpf = _cpfController.text;
+    final String telefone = _telefoneController.text;
+    final String email = _emailController.text;
+    final String rua = _ruaController.text;
+    final String numero = _numeroController.text;
+    final String complemento = _complementoController.text;
+    final String bairro = _bairroController.text;
+    final String cidade = _cidadeController.text;
+    final String estado = _estadoController.text;
+    final String profissao = _profissaoController.text;
+    final double renda = double.parse(_rendaController.text);
+    final String status = _cpfController.text;
 
-    final String nome = _controladorCampoNome.text;
-    final String cpf = _controladorCampoCpf.text;
     if (_formkey.currentState!.validate()) {
-      final clienteCriado = Cliente(nome, cpf);
+      final clienteCriado = Cliente(nome, cpf, telefone, email, rua, numero,
+          complemento, bairro, cidade, estado, profissao, renda, status);
       Navigator.pop(context, clienteCriado);
-    }
-    else {
-      exibirAlerta(context: context, titulo: 'Alerta!', content: 'Erro ao cadastrar o cliente.');
+    } else {
+      exibirAlerta(
+          context: context,
+          titulo: 'Alerta!',
+          content: 'Erro ao cadastrar o cliente.');
     }
   }
 
   Widget _construirFormulario(context) {
-    return Form(
-      key: _formkey,
-      child: Column(
-        children: <Widget>[
-          CampoForm(
-            controlador: _controladorCampoNome,
-            dica: _dicaCampoNome,
-            rotulo: _rotuloCampoNome,
-            validacao: (value) {
-              if(value!.isEmpty) {
-                return 'Informe o nome';
-              }
-            },
-            inputType: TextInputType.name,
-          ),
-
-          const SizedBox(height: 15),
-
-          CampoForm(
-            dica: _dicaCampoCpf,
-            controlador: _controladorCampoCpf,
-            rotulo: _rotuloCampoCpf,
-            tamanhoMaximo: 14,
-            formatos: [
-              FilteringTextInputFormatter.digitsOnly,
-              CpfInputFormatter(),
-            ],
-            validacao: (value) {
-              if(value!.isEmpty) {
-                return 'Informe o CPF';
-              }
-              if(value.length < 14) {
-                return 'CPF inválido';
-              }
-            },
-            inputType: TextInputType.number,
-          ),
-
-          const SizedBox(height: 30),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 16),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => _criaCliente(context),
-                child: const Text(
-                  _textoBotaoConfirmar, style: TextStyle(fontSize: 18.0),),
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Form(
+        key: _formkey,
+        child: Column(
+          children: <Widget>[
+            CampoForm(
+              controlador: _nomeController,
+              dica: _dicaCampoNome,
+              rotulo: _rotuloCampoNome,
+              validacao: (value) {
+                if (value!.isEmpty) {
+                  return 'Nome não pode ficar em branco!';
+                }
+                if (!value.contains(" ")) {
+                  return 'Informe o nome completo';
+                }
+                return null;
+              },
+              inputType: TextInputType.name,
             ),
-          )
-        ],
+            const SizedBox(height: 30),
+            CampoForm(
+              dica: _dicaCampoCpf,
+              controlador: _cpfController,
+              rotulo: _rotuloCampoCpf,
+              tamanhoMaximo: 14,
+              formatos: [
+                FilteringTextInputFormatter.digitsOnly,
+                CpfInputFormatter(),
+              ],
+              validacao: (value) {
+                if (value!.isEmpty) {
+                  return 'CPF não pode ficar em branco!';
+                }
+                if (value.length < 14) {
+                  return 'CPF inválido!';
+                }
+                return null;
+              },
+              inputType: TextInputType.number,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _telefoneController,
+              rotulo: 'Telefone',
+              dica: '(00) 90000-0000',
+              tamanhoMaximo: 15,
+              formatos: [
+                FilteringTextInputFormatter.digitsOnly,
+                TelefoneInputFormatter(),
+              ],
+              validacao: (value) {
+                if (value!.isEmpty) {
+                  return 'Telefone não pode ficar em branco!';
+                }
+                if (value.length < 15) {
+                  return 'Telefone incompleto!';
+                }
+                return null;
+              },
+              inputType: TextInputType.phone,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _emailController,
+              rotulo: 'Email',
+              dica: 'email@email.com',
+              validacao: (value) {
+                if (value!.isEmpty) {
+                  return 'Email não pode ficar em branco!';
+                }
+                if (!value.contains("@") || !value.contains(".")) {
+                  return 'Email inválido!';
+                }
+                return null;
+              },
+              inputType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _profissaoController,
+              rotulo: "Profissão",
+              dica: 'profissão',
+              inputType: TextInputType.text,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _rendaController,
+              rotulo: 'Renda',
+              dica: '00,00',
+              inputType: TextInputType.number,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _statusController,
+              rotulo: 'Status',
+              dica: 'Selecione...',
+              inputType: TextInputType.none,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _ruaController,
+              rotulo: 'Rua',
+              dica: 'Rua A',
+              inputType: TextInputType.none,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _numeroController,
+              rotulo: 'Número',
+              dica: '00',
+              inputType: TextInputType.streetAddress,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _complementoController,
+              rotulo: 'Complemento',
+              dica: 'Bloco A',
+              inputType: TextInputType.streetAddress,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _bairroController,
+              rotulo: 'Bairro',
+              dica: 'Setor A',
+              inputType: TextInputType.streetAddress,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _cidadeController,
+              rotulo: 'Cidade',
+              dica: 'cidade',
+              inputType: TextInputType.streetAddress,
+            ),
+            const SizedBox(height: 30),
+            CampoForm(
+              controlador: _estadoController,
+              rotulo: 'Estado',
+              dica: 'Selecione...',
+              inputType: TextInputType.none,
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 16),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => _criaCliente(context),
+                  child: const Text(
+                    _textoBotaoConfirmar,
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
